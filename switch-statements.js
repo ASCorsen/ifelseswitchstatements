bcb611d7f2cbd934364ea00b05d477868d6576fd
ASCorsen <andrew.corsen@gmail.com>
2021-09-27 16:10:45 +0200

    huiswerk af!

--- switch-statements.js
+++ switch-statements.js
@@ -1,8 +1,8 @@
 // ==========================================
 // Opdracht 1: schrijf een switch statement die op basis van de volgende variabele een bijbehorende foutmelding in de console logt.
 // ==========================================
-
-const errorCode = 0;
+console.log("Koffie apparaat? status: ")
+const errorCode = 4;
 
 // Je mag ervanuit gaan dat deze variabele altijd een waarde tussen de 0 en de 4 bevat. Deze codes hebben de volgende betekenissen (foutmeldingen):
 // 0: "Geen watertoevoer"
@@ -15,7 +15,25 @@ const errorCode = 0;
 // const errorCode = 0 geeft "Geen watertoevoer" in de terminal
 // const errorCode = 4 geeft "Geen druk" in de terminal
 // etc.
-
+switch (errorCode) {
+    case 0:
+        console.log("Geen watertoevoer")
+        break;
+    case 1:
+        console.log("Temperatuur te laag")
+        break
+    case 2:
+        console.log("Koffiebonen op")
+        break
+    case 3:
+        console.log("Afvalbak vol")
+        break
+    case 4:
+        console.log("Geen druk")
+        break
+    default:
+        console.log("Geen foutmeldingen")
+}
 
 // ==========================================
 // Opdracht 2: schrijf een switch statement die op basis van de volgende variabele een bijbehorende foutmelding in de console logt.
@@ -30,6 +48,24 @@ const monitorColor = "red";
 // "orange": "Kritieke hartslag, actie vereist!"
 // "red": "Kritieke hartslag, actie vereist!"
 // Bij alle andere kleuren is de status van de hartslag onbekend.
+console.log("Hartslag monitor Status :")
+
+switch (monitorColor) {
+    case "green":
+        console.log("Hartslag stabiel")
+        break
+    case "yellow":
+        console.log("Hartslag verhoogd")
+        break
+    case "orange":
+        console.log("Kritieke hartslag, actie vereist!")
+        break
+    case "red":
+        console.log("Kritieke hartslag, actie vereist!")
+        break
+    default:
+        console.log("status van de hartslag onbekend")
+}
 
 // Verwachtte uitkomsten:
 // const monitorColor = "red" geeft "Kritieke hartslag, actie vereist!" in de terminal
